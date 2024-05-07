e105f1df85f669fae85c199cce385bd303c13c3f
mishakravchenia <mihailokravchenia@gmail.com>
2024-05-07 14:20:34 +0300

    Add let

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+let product1 = 2;
+let price1 = 100;
+let totalPrice1 = product1 * price1;
+
+let product2 = 180;
+let price2 = 4;
+let totalPrice2 = product2 / price2;
+
+let product3 = 500;
+let price3 = 150;
+let totalPrice3 = product3 + price3;
+console.log("final price=", totalPrice1, totalPrice2, totalPrice3);
+
+let number = 11;
+let boolean = true;
+let string = "java script";
+let stringNumbe = "100";
+console.log(number, boolean, string, string);
