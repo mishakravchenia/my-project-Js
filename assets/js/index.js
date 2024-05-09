8b2c513db482428c21d874add74e384a143fd684
mishakravchenia <mihailokravchenia@gmail.com>
2024-05-09 10:21:22 +0300

    Add const

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,37 @@
-let product1 = 2;
-let price1 = 100;
-let totalPrice1 = product1 * price1;
+// let product1 = 2;
+// let price1 = 100;
+// let totalPrice1 = product1 * price1;
 
-let product2 = 180;
-let price2 = 4;
-let totalPrice2 = product2 / price2;
+// let product2 = 180;
+// let price2 = 4;
+// let totalPrice2 = product2 / price2;
 
-let product3 = 500;
-let price3 = 150;
-let totalPrice3 = product3 + price3;
-console.log("final price=", totalPrice1, totalPrice2, totalPrice3);
+// let product3 = 500;
+// let price3 = 150;
+// let totalPrice3 = product3 + price3;
+// console.log("final price=", totalPrice1, totalPrice2, totalPrice3);
 
-let number = 11;
-let boolean = true;
-let string = "java script";
-let stringNumbe = "100";
-console.log(number, boolean, string, string);
+// let number = 11;
+// let boolean = true;
+// let string = "java script";
+// let stringNumber = "100";
+// console.log(number, boolean, string, stringNumber);
+////////////////////////////////////////////////////////////////////////////////
+
+const number2 = +prompt("Input numer ", "0");
+const numerSquare = number2 ** 2;
+console.log("Total number:", numerSquare);
+
+const housePrice1 = +prompt("Price1", "0");
+const housePrice2 = +prompt("Price2", "0");
+const sum = (housePrice1 + housePrice2) / 2;
+console.log("Total sum:", sum);
+
+const minute = +prompt("Input minute", "0");
+const second = minute * 60;
+console.log("total second:", second);
+
+const greeting = "Hello,";
+const userName = prompt("Input name");
+const massage = greeting + " " + userName;
+document.write("<p>" + "'" + greeting + " " + userName + "! " + "'" + "</p>");
