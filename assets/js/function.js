e09daf6cea0b7c87c516f3cc06954fde1c99e888
mishakravchenia <mihailokravchenia@gmail.com>
2024-05-22 23:00:48 +0300

    add tasks three and four

--- assets/js/function.js
+++ assets/js/function.js
@@ -12,10 +12,43 @@
 // console.log(isWorkingAgePerson(88)); // false
 
 //завданя 2
-function checkMultiplicity(a, b) {
-  return a % b === 0;
+// function checkMultiplicity(a, b) {
+//   return a % b === 0;
+// }
+// console.log(checkMultiplicity(25, 5)); // true
+// console.log(checkMultiplicity(15, 3)); // true
+// console.log(checkMultiplicity(15, 5)); // true
+// console.log(checkMultiplicity(15, 4)); // false
+
+//завданя 3
+/**
+ * Функція для перевірки можливості існування трикутника
+ * @param {number} a - Довжина першої сторони трикутника
+ * @param {number} b - Довжина другої сторони трикутника
+ * @param {number} c - Довжина третьої сторони трикутника
+ * @returns {boolean} - Повертає true, якщо трикутник можливий, і false, якщо ні
+ */
+function canFormTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
 }
-console.log(checkMultiplicity(25, 5)); // true
-console.log(checkMultiplicity(15, 3)); // true
-console.log(checkMultiplicity(15, 5)); // true
-console.log(checkMultiplicity(15, 4)); // false
+console.log(canFormTriangle(3, 4, 5));
+console.log(canFormTriangle(1, 10, 12));
+console.log(canFormTriangle(7, 10, 5));
+console.log(canFormTriangle(1, 2, 3));
+//завданя 4
+function calculateTriangleArea(base, height) {
+  return 0.5 * base * height;
+}
+
+function calculateRectangleArea(length, width) {
+  return length * width;
+}
+let triangleBase = 5;
+let triangleHeight = 10;
+let triangleArea = calculateTriangleArea(triangleBase, triangleHeight);
+console.log(`Площа трикутника: ${triangleArea}`);
+
+let rectangleLength = 4;
+let rectangleWidth = 7;
+let rectangleArea = calculateRectangleArea(rectangleLength, rectangleWidth);
+console.log(`Площа прямокутника: ${rectangleArea}`);
